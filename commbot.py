80407d097c1ce179cfe1325d33cafdb6849a5d4b
Grant Tremblay <granttremblay@gmail.com>
2021-01-30 15:03:36 -0500

    Update commbot.py

--- commbot.py
+++ commbot.py
@@ -20,9 +20,9 @@ def send_slack_message(message, blocks=None):
 
     # KEEP YO TOKEN PRIVATE!
     with open('/Users/grant/.slackbot/slackbot_oauth_token', 'r') as tokenfile:
-        token = tokenfile.readlines()[0]
+        # you need to do this to strip the \n
+        slack_token = tokenfile.read().splitlines()[0]
 
-    slack_token = token
     slack_channel = '#comm_passes'
     slack_icon_url = 'https://avatars.slack-edge.com/2021-01-28/1695804235940_26ef808c676830611f43_512.png'
     slack_user_name = 'HRC CommBot'
@@ -37,16 +37,14 @@ def send_slack_message(message, blocks=None):
         'blocks': json.dumps(blocks) if blocks else None}).json()
 
 
-def grab_critical_telemetry():
+def grab_critical_telemetry(start=CxoTime.now() - 60 * u.s):
 
-    # Repeating myself because now needs to be re-established whenever this is called vOv
-    now = CxoTime.now() - 300 * u.s
-
-    critical_msidlist = ['CCSDSTMF', '2SHEV1RT', '2PRBSCR', '2FHTRMZT']
+    critical_msidlist = ['CCSDSTMF', '2SHEV1RT', '2PRBSCR',
+                         '2FHTRMZT', '2IMTPAST', '2IMBPAST', '2SPTPAST', '2SPBPAST']
     critical_msids = fetch.get_telem(
-        critical_msidlist, start=now, quiet=True, unit_system='eng')
+        critical_msidlist, start=start, quiet=True, unit_system='eng')
 
-    tm_format = critical_msids['CCSDSTMF']
+    tm_format = critical_msids['CCSDSTMF'].vals[-1]
     shield_rate = critical_msids['2SHEV1RT'].vals[-1]
     if shield_rate > 0:
         shield_state = 'UP'
@@ -54,9 +52,13 @@ def grab_critical_telemetry():
         shield_state = 'DOWN'
     bus_current = np.round(critical_msids['2PRBSCR'].vals[-1], 2)
     fea_temp = np.round(critical_msids['2FHTRMZT'].vals[-1], 2)
+    hrc_i_voltage = (
+        critical_msids['2IMTPAST'].vals[-1], critical_msids['2IMBPAST'].vals[-1])
+    hrc_s_voltage = (
+        critical_msids['2SPTPAST'].vals[-1], critical_msids['2SPBPAST'].vals[-1])
 
     telem = {'Format': tm_format, 'Shield Rate': shield_rate, 'Shield State': shield_state,
-             'Bus Current': bus_current, 'FEA Temp': fea_temp}
+             'Bus Current': bus_current, 'FEA Temp': fea_temp, 'HRC-I Voltage Steps': hrc_i_voltage, 'HRC-S Voltage Steps': hrc_s_voltage}
 
     return telem
 
@@ -71,11 +73,9 @@ def main():
 
     # Loop infinitely :)
     while True:
-        # Just grab the last 60 seconds of data to keep the request small and fast
-        now = CxoTime.now() - 60 * u.s
 
         # If there VCDU frame values within the last 60 seconds, this will not be empty
-        ref_vcdu = fetch.Msid('CVCDUCTR', start=now)
+        ref_vcdu = fetch.Msid('CVCDUCTR', start=CxoTime.now() - 60 * u.s)
 
         time.sleep(2)  # These fetches are really fast. Slow the cadence a bit.
 
@@ -84,12 +84,13 @@ def main():
 
             if recently_in_comm is True:
                 # then we've JUST been in comm and we need to report its end.
-                telem = grab_critical_telemetry()
-                message = f"It appears that COMM has ended as of `{now.strftime('% m/%d/%Y % H: % M: % S')}`. Last telemetry was in {telem['Format']}: \n *Shields were {telem['Shield State']}* with a count rate of `{telem['Shield Rate']} cps`. \n *Bus Current* was `{telem['Bus Current']} A` (warning limit is 2.3 A). \n *FEA Temperature* was `{telem['FEA Temp']} C`"
+                telem = grab_critical_telemetry(
+                    start=CxoTime.now() - 1800 * u.s)
+                message = f"It appears that COMM has ended as of `{CxoTime.now().strftime('%m/%d/%Y %H:%M:%S')}`. Last telemetry was in {telem['Format']}: \n *Shields were {telem['Shield State']}* with a count rate of `{telem['Shield Rate']} cps`. \n *Bus Current* was `{telem['Bus Current']} A` (warning limit is 2.3 A). \n *FEA Temperature* was `{telem['FEA Temp']} C`"
 
             recently_in_comm = False
             in_comm_counter = 0
-            print(f'({now.strftime("%m/%d/%Y %H:%M:%S")}) Not in Comm.',
+            print(f'({CxoTime.now().strftime("%m/%d/%Y %H:%M:%S")}) Not in Comm.',
                   end="\r", flush=True)
 
         if len(ref_vcdu) > 0:
@@ -98,16 +99,18 @@ def main():
             in_comm_counter += 1
 
             time.sleep(5)  # Wait a few seconds for MAUDE to refresh
-            latest_vcdu = fetch.Msid('CVCDUCTR', start=now).vals[-1]
+            latest_vcdu = fetch.Msid(
+                'CVCDUCTR', start=CxoTime.now() - 300 * u.s).vals[-1]
 
             print(
-                f'({now.strftime("%m/%d/%Y %H:%M:%S")} | VCDU {latest_vcdu} | #{in_comm_counter}) IN COMM!', end="\r", flush=True)
+                f'({CxoTime.now().strftime("%m/%d/%Y %H:%M:%S")} | VCDU {latest_vcdu} | #{in_comm_counter}) IN COMM!', end="\r", flush=True)
 
             if in_comm_counter == 5:
                 # Now we've waited ~half a minute or so for MAUDE to update
-                telem = grab_critical_telemetry()
+                telem = grab_critical_telemetry(
+                    start=CxoTime.now() - 300 * u.s)
                 # Craft a message string using this latest elemetry
-                message = f"We are now *IN COMM* as of `{now.strftime('% m/%d/%Y % H: % M: % S')}`. \n Telemetry Format = *{telem['Format']}* \n *Shields are {telem['Shield State']}* with a count rate of `{telem['Shield Rate']} cps`. \n *Bus Current* is `{telem['Bus Current']} A` (warning limit is 2.3 A). \n *FEA Temperature* is `{telem['FEA Temp']} C`"
+                message = f"We are now *IN COMM* as of `{CxoTime.now().strftime('%m/%d/%Y %H:%M:%S')}` (_Chandra_ time). \n \n  Telemetry Format = *{telem['Format']}* \n *HRC-I* Voltage Steps (Top/Bottom) = *{telem['HRC-I Voltage Steps'][0]}/{telem['HRC-I Voltage Steps'][1]}* \n *HRC-S* Voltage Steps (Top/Bottom) = *{telem['HRC-S Voltage Steps'][0]}/{telem['HRC-S Voltage Steps'][1]}* \n \n *Shields are {telem['Shield State']}* with a count rate of `{telem['Shield Rate']} cps`. \n *Bus Current* is `{telem['Bus Current']} A` (warning limit is 2.3 A). \n *FEA Temperature* is `{telem['FEA Temp']} C`"
                 # Send the message using our slack bot
                 send_slack_message(message)
 
