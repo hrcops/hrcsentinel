a15997582059f7a2873a8120220a0e7a1618706e
Grant Tremblay <granttremblay@gmail.com>
2023-02-01 12:20:01 -0500

    Update global_configuration.py

--- hrcsentinel/global_configuration.py
+++ hrcsentinel/global_configuration.py
@@ -1,4 +1,5 @@
 allowed_hosts = {'han-v': '/proj/web-icxc/htdocs/hrcops/hrcmonitor/plots/',
+                 'entropy': '/proj/web-icxc/htdocs/hrcops/hrcmonitor/plots/',
                  'hrccdp1': '/home/hrc/HRCSentinel/staging_area/',
                  'gravity': '/Users/grant/Desktop/',
                  'symmetry': '/Users/grant/Desktop/',
