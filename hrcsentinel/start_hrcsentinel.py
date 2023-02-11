fbcf4086e285284f9d9547381c39e0098441b0a5
Grant Tremblay <granttremblay@gmail.com>
2023-02-11 08:28:55 -0500

    Update start_hrcsentinel.py

--- hrcsentinel/start_hrcsentinel.py
+++ hrcsentinel/start_hrcsentinel.py
@@ -4,8 +4,12 @@ import os
 
 hrcsentinel_directory = '/home/tremblay/HRCOps/hrcsentinel/hrcsentinel'
 skainit_string = 'source /proj/sot/ska3/flight/bin/ska_envs.sh'
-script_to_run = 'monitor_comms.py'
 
+scripts_to_run = ['monitor_comms.py', 'monitor_telemetry.py']
+terminal_window_titles = ['Comm Pass Monitor | HRCSentinel',
+                          'Telemetry Monitor | HRCSentinel']
 
-os.system(
-    f'gnome-terminal --title "CommBot" -- bash -c "{skainit_string}; cd {hrcsentinel_directory}; python {script_to_run}"')
+for title, script in zip(terminal_window_titles, scripts_to_run):
+    print(f'Launching {script} in a new terminal window...')
+    os.system(
+        f'gnome-terminal --title "{title}" -- bash -c "{skainit_string}; cd {hrcsentinel_directory}; python {script}"')
